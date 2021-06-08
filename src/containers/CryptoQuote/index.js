6d67fd739c8d9f48dd42bfa0be0ffdc64bf65bf9
Temesghen <mail.temesghen.tekeste@gmail.com>
2021-06-09 02:10:40 +0300

    Update CryptoQuote component; add styles and some details

--- src/containers/CryptoQuote/index.js
+++ src/containers/CryptoQuote/index.js
@@ -1,7 +1,9 @@
+/* eslint-disable react/jsx-one-expression-per-line */
 import { useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { getCryptocurrencyQuoteAsync } from '../../redux/cryptoQuotesSlice';
+import styles from './CryptoQuote.module.css';
 
 const CruyptoQuote = () => {
   const { id } = useParams();
@@ -16,15 +18,40 @@ const CruyptoQuote = () => {
   console.log(cryptoQuote);
 
   return (
-    cryptoQuote && (
-      <div>
-        <h1>
-          CruyptoQuote: **
-          {cryptoQuote[0].name}
-          ** :: **
-          {id}
-          **
-        </h1>
+    cryptoQuote.length && (
+      <div className={styles.cryptoQuoteCard}>
+        <header>
+          <h2>{cryptoQuote[0].name}</h2>
+          <h2>{cryptoQuote[0].symbol}</h2>
+          <h2>${cryptoQuote[0].price}</h2>
+        </header>
+
+        <div className={styles.cryptoQuote__stats}>
+          <div className={styles.cryptoQuote__stats__dayLowHigh}>
+            <span>Day Low: {cryptoQuote[0].dayLow}</span>
+            <span>Day High: {cryptoQuote[0].dayLow}</span>
+          </div>
+
+          <div className="cryptoQuote__stats__yearLowHigh">
+            <span>Year Low {cryptoQuote[0].yearLow}</span>
+            <span>Year High {cryptoQuote[0].yearLow}</span>
+          </div>
+
+          <div className="cryptoQuote__stats__price">
+            <span>Market Cap: {cryptoQuote[0].marketCap}</span>
+            <span>Price (Average 50): {cryptoQuote[0].priceAvg50}</span>
+            <span>Price (Average 200): {cryptoQuote[0].priceAvg200}</span>
+            <span>Volume: {cryptoQuote[0].volume}</span>
+            <span>Average Volume: {cryptoQuote[0].averageVolume}</span>
+          </div>
+        </div>
+
+        <footer>
+          <span>Exchange: {cryptoQuote[0].exchange}</span>
+          <span>Shares Outstanding: {cryptoQuote[0].sharesOutstanding}</span>
+          <span>Open: {cryptoQuote[0].open}</span>
+          <span>Previous Close: {cryptoQuote[0].previousClose}</span>
+        </footer>
       </div>
     )
   );
