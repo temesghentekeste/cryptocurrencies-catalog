d02411162bcce3fc1653c0555a718185c87d67dc
Temesghen <mail.temesghen.tekeste@gmail.com>
2021-06-10 02:16:09 +0300

    Combine trending reducer to the root reducer in the store; update ui

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import cryptocurrenciesReducer from './cryptocurrenciesSlice';
 import cryptocurrencyQuoteReducer from './cryptoQuotesSlice';
+import cryptocurrencyTrendingReducer from './trendingSlice';
 
 export default configureStore({
   reducer: {
     cryptocurrencies: cryptocurrenciesReducer,
     cryptocurrencyquote: cryptocurrencyQuoteReducer,
+    trendingcryptocurrencies: cryptocurrencyTrendingReducer,
   },
 });
